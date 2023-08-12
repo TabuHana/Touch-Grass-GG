0be1922a939cd351da0b86a1394be23f97ed9e84
tabu <taboodog@hotmail.com>
2023-08-11 17:53:38 -0700

    chore: health check

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import config from 'config';
 import logger from './utils/logger';
 import createServer from './utils/server';
+import routes from './routes';
 
 // variables
 const port = config.get('port');
@@ -14,4 +15,12 @@ const app = createServer();
 // start server
 app.listen(port, async () => {
     logger.info(`App is running at http://localhost:${port}`);
+
+    /**
+     * @to-do
+     * add db connection
+     * await connect()
+     */
+
+    routes(app);
 });
